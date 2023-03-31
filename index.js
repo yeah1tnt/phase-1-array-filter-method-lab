d84ed96ec10e2ab7efd0329479247cf07d977661
Tong Tang <pichip22@hotmail.com>
2023-03-31 18:36:39 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(array, name){
+    const newArray = [];
+    for(let i in array){
+        if(array[i].toLowerCase() === name.toLowerCase()){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+
+function fuzzyMatch(array,name){
+    const newArray = [];
+    for(let i  = 0; i < array.length;i++){
+        if(array[i].slice(0,1).toLowerCase() === name.slice(0,1).toLowerCase()){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+
+function matchName(array,name){
+    const newArray = array.filter(array => array.name === name);
+    return newArray;
+}
